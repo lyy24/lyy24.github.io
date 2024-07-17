37a30972d3d9f8dd6d1a8e6e8d59a4a24fac6b3f
lyy24 <xr_007@i.ua>
2024-07-17 20:40:32 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -29,12 +29,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'iskra.od@gmail.com';                 // Наш логин
+$mail->Username = '';                 // Наш логин
 $mail->Password = ' ';                                // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('iskra.od@gmail.com', 'Top Forum');   // От кого письмо 
+$mail->setFrom('', 'Top Forum');   // От кого письмо 
 $mail->addAddress(' ');                              // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
